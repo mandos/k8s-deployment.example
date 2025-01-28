9dce336cdd04b4d72bc8c99bd4a76a79947f1029
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2025-01-28 17:56:49 +0900

    fix: Cache for Vault operator

--- TODO.md
+++ TODO.md
@@ -2,4 +2,5 @@
 
  - Move vault static secret to backend chart
  - Set testing automatically or not
+ - Write documentation
 
--- vault/init.sh
+++ vault/init.sh
@@ -19,4 +19,23 @@ vault write auth/vis/role/app1 \
     audience=vault \
     ttl=24h
 
+vault secrets enable -path=vis-transit transit
+tee /tmp/cache.hcl <<EOF
+path "vis-transit/encrypt/vso-client-cache" {
+   capabilities = ["create", "update"]
+}
+path "vis-transit/decrypt/vso-client-cache" {
+   capabilities = ["create", "update"]
+}
+EOF
+
+vault policy write vault-operator-cache /tmp/cache.hcl
+
+vault write auth/vis/role/auth-role-operator \
+    bound_service_account_names=vault-operator-vault-secrets-operator-controller-manager \
+    bound_service_account_namespaces=vault \
+    policies=vault-operator-cache \
+    audience=vault \
+    ttl=24h
+
 vault kv put vis/database DB_HOST="database" DB_USER="app1_user" DB_PASSWORD="app1_password"
